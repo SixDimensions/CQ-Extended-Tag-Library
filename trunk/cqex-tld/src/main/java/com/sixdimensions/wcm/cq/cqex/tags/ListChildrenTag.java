5d78d39a994071a5590189a75057575513e7c6c8
dan.klco <dan.klco@188c018c-155f-4d0b-a3fe-d0394b648509>
2012-07-30 18:04:56 +0000

    Initial Code Import

---
+++ trunk/cqex-tld/src/main/java/com/sixdimensions/wcm/cq/cqex/tags/ListChildrenTag.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012 - Six Dimensions
+ */
+package com.sixdimensions.wcm.cq.cqex.tags;
+
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.Tag;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.apache.sling.api.resource.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sixdimensions.wcm.cq.cqex.util.IterableIterator;
+
+/**
+ * Tag for listing the children of a resource. The children are saved into the
+ * page context as a variable with the name var and are saved as an
+ * IterableIterator, which means they can be iterated through JSTL.
+ * 
+ * @author dklco
+ */
+public class ListChildrenTag extends TagSupport {
+	private static final long serialVersionUID = 5861756752614447760L;
+	private static final Logger log = LoggerFactory
+			.getLogger(ListChildrenTag.class);
+	private Resource resource;
+	private String var = "resources";
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.jsp.tagext.TagSupport#doEndTag()
+	 */
+	@Override
+	public int doEndTag() {
+		ListChildrenTag.log.trace("doEndTag");
+		IterableIterator<Resource> children = new IterableIterator<Resource>(
+				this.resource.listChildren());
+		this.pageContext.setAttribute(this.var, children,
+				PageContext.REQUEST_SCOPE);
+		return Tag.EVAL_PAGE;
+	}
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
+
+	public void setVar(String var) {
+		this.var = var;
+	}
+}
