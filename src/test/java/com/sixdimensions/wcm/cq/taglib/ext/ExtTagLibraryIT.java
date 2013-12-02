9c5fb45d91baf573bcd1120b032c7dc03541bfbf
Dan Klco <dan.klco@sixdimensions.com>
2013-12-02 01:31:01 -0500

    Adding a test for the display mode (which does not seem to be working) and fixing the defer test script

--- src/test/java/com/sixdimensions/wcm/cq/taglib/ext/ExtTagLibraryIT.java
+++ src/test/java/com/sixdimensions/wcm/cq/taglib/ext/ExtTagLibraryIT.java
@@ -15,6 +15,8 @@
  */
 package com.sixdimensions.wcm.cq.taglib.ext;
 
+import static org.junit.Assert.assertTrue;
+
 import org.apache.sling.testing.tools.sling.SlingClient;
 import org.apache.sling.testing.tools.sling.SlingTestBase;
 import org.junit.Before;
@@ -106,5 +108,55 @@ public class ExtTagLibraryIT extends SlingTestBase {
 				.assertStatus(200).assertContentContains("twoone");
 
 		log.info("All Defer tests successful");
+	}
+
+	/**
+	 * Test the two defer tags
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testDisplayMode() throws Exception {
+
+		log.info("Creating testing component...");
+		Utils.createFolders(this.slingClient, "/apps/test/ext/displayMode");
+		this.slingClient.upload(
+				"/apps/test/ext/displayMode/displayMode.jsp",
+				ExtTagLibraryIT.class.getClassLoader().getResourceAsStream(
+						"displayMode.jsp"), -1, true);
+		log.info(this
+				.getRequestExecutor()
+				.execute(
+						this.getRequestBuilder()
+								.buildGetRequest(
+										"/apps/test/ext/displayMode.3.json")
+								.withCredentials("admin", "admin"))
+				.assertStatus(200).getContent());
+
+		log.info("Creating testing content...");
+		Utils.createFolders(this.slingClient, "/content/test/ext");
+		this.slingClient.createNode("/content/test/ext/displayMode",
+				"jcr:primaryType", "nt:unstructured", "sling:resourceType",
+				"test/ext/displayMode");
+		log.info(this
+				.getRequestExecutor()
+				.execute(
+						this.getRequestBuilder()
+								.buildGetRequest(
+										"/content/test/ext/displayMode.json")
+								.withCredentials("admin", "admin"))
+				.assertStatus(200).getContent());
+
+		log.info("Asserting displayMode works");
+		assertTrue(this
+				.getRequestExecutor()
+				.execute(
+						this.getRequestBuilder()
+								.buildGetRequest(
+										"/content/test/ext/displayMode.html")
+								.withCredentials("admin", "admin"))
+				.assertStatus(200).getContent().trim().length() == 0);
+
+		log.info("All displayMode tests successful");
 	}
 }
--- src/test/resources/defer.jsp
+++ src/test/resources/defer.jsp
@@ -1,3 +1,4 @@
 <%@include file="/libs/foundation/global.jsp"%>
+<%@include file="/libs/wcm/core/components/init/init.jsp"%>
 <%@taglib prefix="ext" uri="http://labs.sixdimensions.com/tld/ext" %>
-<ext:defer id="1">one</ext:defer>two<ext:writeDeferred />
+<ext:displayMode mode="author">Author</ext:displayMode><ext:displayMode mode="disabled">Publish</ext:displayMode>
